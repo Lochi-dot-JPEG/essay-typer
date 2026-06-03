092cd5ac02f5c8dc5d7aab714d83f01e0e48548d
Lochie <95149116+Lochi-dot-JPEG@users.noreply.github.com>
2026-06-03 21:53:20 +1000

    fix: correct strikethrough syntax

--- web/script.js
+++ web/script.js
@@ -2,6 +2,8 @@ let textAreaElement = document.getElementById("TypeArea");
 let last_text = "";
 textAreaElement.setAttribute("oninput", "UpdatedText()");
 let backSpaceSize = 0;
+// Strikethrough markdown
+const st_length = 2
 
 function UpdatedText() {
 	let new_text = textAreaElement.value;
@@ -30,77 +32,43 @@ function Backspace() {
 
 		while (searching) {
 			let searched_character = last_text.substring(
-				last_text.length - deletion_length - 2,
-				last_text.length - deletion_length - 1,
+				last_text.length - deletion_length - st_length - 1,
+				last_text.length - deletion_length - st_length,
 			);
 
 			if (searched_character == "~") {
-				last_text = last_text.substring(0, last_text.length - index);
-				for (var i = 0; i < index; i++) {
-					last_text += "~";
-				}
+				last_text = last_text.substring(0, last_text.length - index - 1);
 				searching = false;
 				continue
 			} else {
 				deletion_length += 1;
 			}
 			// Avoid infinite loop if bugged
-			if (deletion_length > 15) {
+			if (deletion_length > 1024) {
 				console.log("overflowed")
 				searching = false;
 			}
 		}
 		// Add another character to the deletion
 		let already_deleted = last_text.substring(
-			last_text.length - deletion_length - 1,
-			last_text.length - 1,
+			last_text.length - deletion_length,
+			last_text.length,
 		);
 		let before_deletion = last_text.substring(
 			0,
-			last_text.length - deletion_length - 3,
+			last_text.length - deletion_length - st_length - 1,
 		);
 		let add_to_deletion = last_text.substring(
-			last_text.length - deletion_length - 2,
-			last_text.length - deletion_length - 3,
+			last_text.length - deletion_length - st_length,
+			last_text.length - deletion_length - st_length - 1,
 		);
-		last_text = before_deletion + "~" + add_to_deletion + already_deleted + "~"
-
-
-
+		last_text = before_deletion + "~~" + add_to_deletion + already_deleted + "~~"
 		// First character deletion
 	} else {
-		let second_last_letter = last_text.substring(
-			last_text.length - 2,
-			last_text.length - 1,
-		);
+		// Delete character
 		last_text = last_text.substring(0, last_text.length - 1);
-		if (second_last_letter != "~") {
-			last_text = last_text + "~";
-		} else {
-			last_text = last_text.substring(0, last_text.length - 1);
-		}
-		last_text = last_text + last_letter + "~";
-
+		last_text = last_text + "~~";
+		last_text = last_text + last_letter + "~~";
 	}
-
-	/*
-	while (searching) {
-	  let searched_character = last_text.substring(
-	    last_text.length - index,
-	    last_text.length - index + 1,
-	  );
-	  if (searched_character != "~") {
-	    last_text = last_text.substring(0, last_text.length - index);
-	    for (var i = 0; i < index; i++) {
-	      last_text += "~";
-	    }
-	    searching = false;
-	  }
-	  index += 1;
-	  if (index > 15) {
-	    searching = false;
-	  }
-	}
-		      */
 	textAreaElement.value = last_text;
 }
