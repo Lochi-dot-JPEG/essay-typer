fe5e35601aae2fc792ae4f3c5159a0d6b23d7e90
Lochie <95149116+Lochi-dot-JPEG@users.noreply.github.com>
2026-06-04 12:57:34 +1000

    fix: replace double strikethrough with nothing

--- web/script.js
+++ web/script.js
@@ -1,112 +1,108 @@
 let textAreaElement = document.getElementById("TypeArea");
-let markedOut = document.getElementById('markedout');
+let markedOut = document.getElementById("markedout");
 let last_text = "";
 textAreaElement.setAttribute("oninput", "UpdatedText()");
 markedOut.setAttribute("onclick", "FocusTyping()");
 let backSpaceSize = 0;
 // Strikethrough markdown
-const st_length = 2
-showdown.setOption('strikethrough', 'true');
+const st_length = 2;
+showdown.setOption("strikethrough", "true");
 let converter = new showdown.Converter();
 
-
-
-textAreaElement.focus()
+textAreaElement.focus();
 function FocusTyping() {
-	textAreaElement.focus()
+  textAreaElement.focus();
 }
 function UpdatedText() {
-	let new_text = textAreaElement.value;
+  let new_text = textAreaElement.value;
 
-	// Backspace crossing out handling
-	let cut_last_text = last_text.substring(0, last_text.length - 1);
-	if (new_text == cut_last_text) {
-		Backspace();
-	} else if (new_text.includes(last_text)) {
-		last_text = new_text;
-		// Create double newlines
-		if (new_text.substring(new_text.length - 1) == "\n") {
-			last_text += "\n"
-		}
-	}
-	MergeDeletions()
-	textAreaElement.value = last_text;
-	RenderMarkdown()
-
+  // Backspace crossing out handling
+  let cut_last_text = last_text.substring(0, last_text.length - 1);
+  if (new_text == cut_last_text) {
+    Backspace();
+  } else if (new_text.includes(last_text)) {
+    last_text = new_text;
+    // Create double newlines
+    if (new_text.substring(new_text.length - 1) == "\n") {
+      last_text += "\n";
+    }
+  }
+  MergeDeletions();
+  textAreaElement.value = last_text;
+  RenderMarkdown();
 }
 
 function RenderMarkdown() {
-	//converter.text = '# hello, markdown!',
-	markedOut.innerHTML = converter.makeHtml(last_text + "|");
-	//markedOut.innerHTML =
-	//marked.parse(last_text + "|");
+  //converter.text = '# hello, markdown!',
+  markedOut.innerHTML = converter.makeHtml(last_text + "|");
+  //markedOut.innerHTML =
+  //marked.parse(last_text + "|");
 }
 
 function Backspace() {
-	let index = 1;
+  let index = 1;
 
-	// Check has already backspaced
-	let last_letter = last_text.substring(last_text.length - 1);
-	if (last_letter == "\n") {
-		return;
-	}
-	let already_deleting = last_letter == "~";
-	// Extend deletion
-	if (already_deleting) {
-		let deletion_length = 1;
-		let searching = true;
+  // Check has already backspaced
+  let last_letter = last_text.substring(last_text.length - 1);
+  if (last_letter == "\n") {
+    return;
+  }
+  let already_deleting = last_letter == "~";
+  // Extend deletion
+  if (already_deleting) {
+    let deletion_length = 1;
+    let searching = true;
 
-		while (searching) {
-			let searched_character = last_text.substring(
-				last_text.length - deletion_length - st_length - 1,
-				last_text.length - deletion_length - st_length,
-			);
+    while (searching) {
+      let searched_character = last_text.substring(
+        last_text.length - deletion_length - st_length - 1,
+        last_text.length - deletion_length - st_length,
+      );
 
-			if (searched_character == "~") {
-				last_text = last_text.substring(0, last_text.length - index - 1);
-				searching = false;
-				continue
-			} else {
-				deletion_length += 1;
-			}
-			// Avoid infinite loop if bugged
-			if (deletion_length > 1024) {
-				console.log("overflowed")
-				searching = false;
-			}
-		}
-		// Add another character to the deletion
-		let already_deleted = last_text.substring(
-			last_text.length - deletion_length,
-			last_text.length,
-		);
-		let before_deletion = last_text.substring(
-			0,
-			last_text.length - deletion_length - st_length - 1,
-		);
-		let add_to_deletion = last_text.substring(
-			last_text.length - deletion_length - st_length,
-			last_text.length - deletion_length - st_length - 1,
-		);
-		if (add_to_deletion == "\n") {
-			last_text += "~~"
-		} else {
-			last_text = before_deletion + "~~" + add_to_deletion + already_deleted + "~~"
-		}
+      if (searched_character == "~") {
+        last_text = last_text.substring(0, last_text.length - index - 1);
+        searching = false;
+        continue;
+      } else {
+        deletion_length += 1;
+      }
+      // Avoid infinite loop if bugged
+      if (deletion_length > 1024) {
+        console.log("overflowed");
+        searching = false;
+      }
+    }
+    // Add another character to the deletion
+    let already_deleted = last_text.substring(
+      last_text.length - deletion_length,
+      last_text.length,
+    );
+    let before_deletion = last_text.substring(
+      0,
+      last_text.length - deletion_length - st_length - 1,
+    );
+    let add_to_deletion = last_text.substring(
+      last_text.length - deletion_length - st_length,
+      last_text.length - deletion_length - st_length - 1,
+    );
+    if (add_to_deletion == "\n") {
+      last_text += "~~";
+    } else {
+      last_text =
+        before_deletion + "~~" + add_to_deletion + already_deleted + "~~";
+    }
 
-		// First character deletion
-	} else {
-		// Delete one character
-
-		last_text = last_text.substring(0, last_text.length - 1);
-		last_text = last_text + "~~";
-		last_text = last_text + last_letter + "~~";
-	}
+    // First character deletion
+  } else {
+    // Delete one character
+    last_text = last_text.substring(0, last_text.length - 1);
+    last_text = last_text + "~~";
+    last_text = last_text + last_letter + "~~";
+  }
 }
 
-
 function MergeDeletions() {
-	last_text = last_text.replaceAll('~~~~', '~~');
-	last_text = last_text.replaceAll('\n\n\n', '\n\n');
-	//last_text = last_text.replaceAll(/'\n\n'/g, '\n');
+  last_text = last_text.replaceAll("~~~~", "");
+  last_text = last_text.replaceAll("\n\n\n", "\n\n");
+  //last_text = last_text.replaceAll(/'\n\n'/g, '\n');
 }
